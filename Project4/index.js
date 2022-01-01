1f8c8e0121ef81d1eacd1003835bc310fead3ffb
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2021-12-31 19:52:16 -0500

    Added classes required and selected them.

---
+++ Project4/index.js
@@ -0,0 +1,9 @@
+const score = document.querySelector(".score");
+const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
+
+startScreen.addEventListener("click", start);
+
+function start(){
+    
+}
