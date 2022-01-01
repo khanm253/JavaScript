b30513ee168c2620bf987c34d480b5131c1a73bb
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2021-12-31 20:00:49 -0500

    Key pressing mechanism complete.

--- Project4/index.js
+++ Project4/index.js
@@ -2,8 +2,30 @@ const score = document.querySelector(".score");
 const startScreen = document.querySelector(".startScreen");
 const gameArea = document.querySelector(".gameArea");
 
+let keys = {
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowLeft:false,
+    ArrowRight:false,
+};
+
 startScreen.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
+
 
 function start(){
-    
+    //console.log('clicked')
+}
+
+function pressOn(e){
+    e.preventDefault();
+    keys[e.key] = true; 
+    console.log(keys)
+}
+
+function pressOff(e){
+    e.preventDefault();
+    keys[e.key] = false; 
+    console.log(keys)
 }
