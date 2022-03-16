999de6f816a5092e5fa97118da6e617128e0e862
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-03-15 23:49:51 -0400

    Added new endpoint

--- Node/rest-api.js
+++ Node/rest-api.js
@@ -9,5 +9,9 @@ app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
 
+app.get('/api/courses/:id/:name', (req, res) => {
+    res.send(req.params);
+});
+
 const port  = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}...`))
