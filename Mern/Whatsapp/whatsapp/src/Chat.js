fae0bc1e21354ec684b9387d54ece868f9670658
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-05-19 18:04:42 -0400

    Completed initial frontend

--- Mern/Whatsapp/whatsapp/src/Chat.js
+++ Mern/Whatsapp/whatsapp/src/Chat.js
@@ -1,10 +1,57 @@
 import React from 'react'
 import './Chat.css'
+import { Avatar, IconButton } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoodIcon from '@material-ui/icons/Mood';
+import MicIcon from '@material-ui/icons/Mic';
 
 function Chat() {
   return (
     <div className='chat'>
-        <h1>I am the chat</h1>
+        <div className='chat-header'>
+          <Avatar src='http://images.clipartpanda.com/avatars-clipart-generic-avatar.png'/>
+          <div className='chat-header-info'>
+            <h3>Room name</h3>
+            <p>Last seen at...</p>
+          </div>
+
+            <div className='chat-header-right'>
+            <IconButton> <SearchIcon/> </IconButton>
+            <IconButton> <AttachFileIcon/> </IconButton>
+            <IconButton> <MoreVertIcon/> </IconButton>
+          </div>
+        </div>
+
+        <div className='chat-body'>
+          <p className='chat-message'>
+            <span className='chat-name'>Abd</span>
+            This is a message
+            <span className='chat-timestamp'>{ new Date().toUTCString() }</span>
+          </p>
+
+          <p className='chat-message chat-receiver'>
+            <span className='chat-name'>Abd</span>
+            This is a message
+            <span className='chat-timestamp'>{ new Date().toUTCString() }</span>
+          </p>
+
+          <p className='chat-message'>
+            <span className='chat-name'>Abd</span>
+            This is a message
+            <span className='chat-timestamp'>{ new Date().toUTCString() }</span>
+          </p>
+        </div>
+
+        <div className='chat-footer'>
+          <MoodIcon/>
+          <form>
+            <input type='text' placeholder='Type a message'/>
+            <button type='submit' >Send</button>
+          </form>
+          <MicIcon/>
+        </div>
     </div>
   )
 }
--- Mern/Whatsapp/whatsapp/src/SidebarChat.js
+++ Mern/Whatsapp/whatsapp/src/SidebarChat.js
@@ -1,8 +1,16 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
+import './SidebarChat.css'
 
 function SidebarChat() {
   return (
-    <div>SidebarChat</div>
+    <div className='side-bar-chat'>
+        <Avatar src='http://images.clipartpanda.com/avatars-clipart-generic-avatar.png'/>
+        <div className='side-bar-chat-info'>
+            <h2 className='room-name'>Room name</h2>
+            <p className='latest-message'>This is the latest message</p>
+        </div>
+    </div>
   )
 }
 
