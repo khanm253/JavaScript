0c5eeb96fb498bb38e728531f93b994f72b3c19e
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-05-21 12:01:17 -0400

    Adding frontend changes

--- Mern/Whatsapp/whatsapp/src/App.js
+++ Mern/Whatsapp/whatsapp/src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { useEffect, useState } from 'react';
+import Pusher from 'pusher-js';
+import axios from './axios'
 
 function App() {
+  
+  const [messages, setmessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('messages/sync').then((response) => {setmessages(response.data)});
+  }, []);
+  
+  useEffect(() => {
+    const pusher = new Pusher('be4562462bd1c38f2d3d', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      setmessages([...messages, data]);
+    });
+
+    return () => {
+      channel.unsubscribe();
+      channel.unbind_all();
+    }
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className='app__body'>
         <Sidebar/>
-        <Chat/>
+        <Chat messages={messages}/>
       </div>
     </div>
   );
--- Mern/Whatsapp/whatsapp/src/Chat.js
+++ Mern/Whatsapp/whatsapp/src/Chat.js
@@ -6,8 +6,34 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoodIcon from '@material-ui/icons/Mood';
 import MicIcon from '@material-ui/icons/Mic';
+import { useState, useEffect } from 'react';
+import axios from './axios'
 
-function Chat() {
+function Chat({ messages }) {
+  const [input, setinput] = useState('');
+  
+  useEffect(() => {
+    let chatBody = document.querySelector('.chat-body');
+    console.log(chatBody)
+    chatBody.scrollTop = chatBody.scrollHeight;
+  }, [messages])
+
+  function isReceiver(received){
+    return received ? 'chat-message chat-receiver' : 'chat-message';
+  }
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    await axios.post('message/new', {
+      message : input,
+      name : "From",
+      timestamp : "Just now",
+      received : false
+    });
+
+    setinput('');
+  };
+
   return (
     <div className='chat'>
         <div className='chat-header'>
@@ -24,31 +50,21 @@ function Chat() {
           </div>
         </div>
 
-        <div className='chat-body'>
-          <p className='chat-message'>
-            <span className='chat-name'>Abd</span>
-            This is a message
-            <span className='chat-timestamp'>{ new Date().toUTCString() }</span>
-          </p>
-
-          <p className='chat-message chat-receiver'>
-            <span className='chat-name'>Abd</span>
-            This is a message
-            <span className='chat-timestamp'>{ new Date().toUTCString() }</span>
-          </p>
-
-          <p className='chat-message'>
-            <span className='chat-name'>Abd</span>
-            This is a message
-            <span className='chat-timestamp'>{ new Date().toUTCString() }</span>
-          </p>
+        <div id='chat-body' className='chat-body'>
+          {messages.map(message => (
+              <p className={isReceiver(message.received)}>
+                <span className='chat-name'>{message.name}</span>
+                {message.message}
+                <span className='chat-timestamp'>{ message.timestamp }</span>
+            </p> 
+          ))}
         </div>
 
         <div className='chat-footer'>
           <MoodIcon/>
           <form>
-            <input type='text' placeholder='Type a message'/>
-            <button type='submit' >Send</button>
+            <input value={input} onChange={e => setinput(e.target.value)} type='text' placeholder='Type a message'/>
+            <button onClick={sendMessage} type='submit' >Send</button>
           </form>
           <MicIcon/>
         </div>
